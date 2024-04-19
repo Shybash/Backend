c03701690d384aaa242ee95c79921cb0aca3fbf1
Shybash <shybashshaik@gmail.com>
2024-04-19 15:58:02 +0530

    updated

---
+++ Controllers/ClubMembers.js
@@ -0,0 +1,36 @@
+const ClubMember = require('../models/ClubMember');
+
+const GetClubMembers = async (req, res) => {
+  try {
+    const aggregatedClubMembers = await ClubMember.aggregate([
+      // Group by club field
+      { $group: { _id: '$club', members: { $push: '$$ROOT' } } }
+    ]);
+    
+    // Construct the groupedClubMembers object
+    const groupedClubMembers = {};
+    aggregatedClubMembers.forEach(({ _id, members }) => {
+      groupedClubMembers[_id] = members;
+    });
+    
+    res.json(groupedClubMembers);
+  } catch (error) {
+    console.error('Error fetching club members:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+const deleteClubMember = async (req, res) => {
+  try {
+    const { clubName, memberId } = req.params;
+    await ClubMember.findOneAndDelete({ club: clubName, _id: memberId });
+    res.status(200).json({ message: 'Club member deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting club member:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+module.exports = { GetClubMembers,deleteClubMember};
--- routes/CollegeRouter.js
+++ routes/CollegeRouter.js
@@ -1,13 +1,15 @@
+// Router file (CollegeRouter.js)
 const express = require('express');
 const router = express.Router();
 const ClgLoginController = require('../Controllers/ClgLogin-controller');
 const ClgRegisterController = require('../Controllers/ClgRegister-controller');
 const { StudentForm } = require('../Controllers/StudentForm');
-const {CreateClub}=require('../Controllers/Club-controller');
+const { CreateClub } = require('../Controllers/Club-controller');
 const GetClubs = require('../Controllers/GetClubs');
 const { AcceptStudent } = require('../Controllers/AcceptStudent');
 const deleteStudent = require('../Controllers/deleteStudent');
-const { GetClubMembers } = require('../Controllers/GetClubMembers');
+const { GetClubMembers, deleteClubMember } = require('../Controllers/ClubMembers');
+
 // Register route
 router.post('/RegisterClg', ClgRegisterController);
 
@@ -15,20 +17,20 @@ router.post('/RegisterClg', ClgRegisterController);
 router.post('/LoginClg', ClgLoginController);
 
 // Student list route
-router.get('/StudentForm',StudentForm);
+router.get('/StudentForm', StudentForm);
 
 // Route for creating a club
 router.post('/CreateClub', CreateClub);
 
-router.get('/GetClubs',GetClubs);
+router.get('/GetClubs', GetClubs);
 
-router.post('/AcceptStudent/:id',AcceptStudent);
+router.post('/AcceptStudent/:id', AcceptStudent);
 
 // Delete student route
-router.delete('/deleteStudent/:id',deleteStudent); // Use the deleteStudent function
+router.delete('/deleteStudent/:id', deleteStudent); // Use the deleteStudent function
 
-router.get('/GetClubMembers',GetClubMembers);
+router.get('/GetClubMembers', GetClubMembers);
 
-
+router.delete('/deleteClubMember/:clubName/:memberId', deleteClubMember);
 
 module.exports = router;
