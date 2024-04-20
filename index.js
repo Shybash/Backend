957da35daa29f0d57f1dc36ccb7134a48a568424
Shybash <shybashshaik@gmail.com>
2024-04-20 15:13:34 +0530

    querys

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const cors=require('cors');
 require('dotenv').config();
-const connection = require("./db");
-const studentRoutes = require('./routes/StudentRoutes');
-const collegeRouters = require('./routes/CollegeRouter');
+const connection=require("./db");
+const studentRoutes=require('./routes/StudentRoutes');
+const collegeRouters=require('./routes/CollegeRouter');
 
-// Middleware
-app.use(cors()); // Allow requests from any origin
-app.use(bodyParser.json()); // Parse JSON bodies
-
-// Database connection
+//middleware
+app.use(express.json());
+app.use(cors({origin:"*"}));
+app.use(bodyParser.json());
+//database connection
 connection();
 
-// Routes
-app.use("/api", studentRoutes);
-app.use("/api", collegeRouters);
+//Routes
+app.use("/api",studentRoutes);
+app.use("/api",collegeRouters);
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => { console.log(`Server is running on ${port}`) });
+const port=process.env.PORT || 4000;
+app.listen(port,()=>{console.log(`server is running on ${port}`)});
