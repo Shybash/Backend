aa2ce056a307695b4f6f00513a0c8575bb3268ef
Shybash <shybashshaik@gmail.com>
2024-04-20 14:30:04 +0530

    querys

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const collegeRouters=require('./routes/CollegeRouter');
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(bodyParser.json());
 //database connection
 connection();
