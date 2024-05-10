746df634c98cca0aa330fbab727ede6f28a81bcf
Shybash <shybashshaik@gmail.com>
2024-05-10 22:57:45 +0530

    updated

--- Controllers/StudentForm.js
+++ Controllers/StudentForm.js
@@ -2,22 +2,26 @@
 const Student = require('../models/StudentForm'); // Assuming you have a Student model defined
 
 // Define the controller function to fetch student form data grouped by club
-const StudentForm= async (req, res) => {
+const StudentForm = async (req, res) => {
   try {
-    // Fetch student form data from the database
-    const students = await Student.find();
-
-    // Group students by club
-    const groupedStudents = students.reduce((acc, student) => {
-      if (!acc[student.club]) {
-        acc[student.club] = [];
+    // Aggregate to group students by club
+    const groupedStudents = await Student.aggregate([
+      {
+        $group: {
+          _id: '$club',
+          students: { $push: '$$ROOT' }
+        }
       }
-      acc[student.club].push(student);
-      return acc;
-    }, {});
+    ]);
+
+    // Construct the response object
+    const formattedData = {};
+    groupedStudents.forEach(group => {
+      formattedData[group._id] = group.students;
+    });
 
     // Respond with the fetched data
-    res.json(groupedStudents);
+    res.json(formattedData);
   } catch (error) {
     // If an error occurs, respond with an error message
     console.error('Error fetching student form data:', error);
