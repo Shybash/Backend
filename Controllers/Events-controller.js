65ddda2bdd2fc43870349f56f3460381ae2a0fde
Shybash <shybashshaik@gmail.com>
2024-05-12 14:34:01 +0530

    updated

---
+++ Controllers/Events-controller.js
@@ -0,0 +1,25 @@
+// controllers/eventController.js
+const Event = require('../models/Event');
+
+const addEvent = async (req, res) => {
+  try {
+    const { name, date, time, ampm, location, description } = req.body;
+    const event = new Event({
+      name,
+      date,
+      time: `${time} ${ampm}`,
+      location,
+      description,
+    });
+    await event.save();
+    console.log('Event added:', event);
+    res.status(201).send('Event added successfully');
+  } catch (error) {
+    console.error('Error adding event:', error);
+    res.status(500).send('Error adding event');
+  }
+};
+
+module.exports = {
+  addEvent,
+};
--- models/Event.js
+++ models/Event.js
@@ -1,14 +1,12 @@
-// Event.js (in your models directory)
-
+// models/Event.js
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  date: { type: Date, required: true },
-  time: { type: String, required: true },
-  ampm: { type: String, required: true }, // Add ampm field to store "AM" or "PM"
-  venue: { type: String, required: true },
-  description: { type: String, required: true }
+  name: {type: String,required: true},
+  date: {type: Date,required: true},
+  time: { type: String,required: true},
+  location: {type: String,required: true},
+  description: {type: String,required: true}
 });
 
 const Event = mongoose.model('Event', eventSchema);
