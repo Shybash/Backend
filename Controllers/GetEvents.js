44ce0c1aa943818fabc349d559752af78d35c221
Shybash <shybashshaik@gmail.com>
2024-05-12 22:47:05 +0530

    updated

--- Controllers/GetEvents.js
+++ Controllers/GetEvents.js
@@ -1,9 +1,23 @@
 // eventsController.js
 const Event = require('../models/Event');
 
+// Function to delete expired events
+const deleteExpiredEvents = async () => {
+  try {
+    const currentTime = new Date();
+    await Event.deleteMany({ date: { $lt: currentTime } });
+    console.log('Expired events deleted successfully.');
+  } catch (error) {
+    console.error('Error deleting expired events:', error);
+  }
+};
+
 // Function to fetch all events
 const getEvents = async (req, res) => {
   try {
+    // Call deleteExpiredEvents before fetching events
+    await deleteExpiredEvents();
+
     const events = await Event.find();
     res.status(200).json(events);
   } catch (error) {
@@ -12,4 +26,4 @@ const getEvents = async (req, res) => {
   }
 };
 
-module.exports = { getEvents };
+module.exports = { getEvents};
