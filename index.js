6c97cebbc7e5c4050d65ff2084c8b6cda1a3ea85
Shybash <shybashshaik@gmail.com>
2024-06-20 14:32:25 +0530

    added Oauth

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://frontend-clubhub-virid.vercel.app",
   credentials: true
 }));
 
