79301a92490626e8ced039776f4edf62804b512d
Shybash <shybashshaik@gmail.com>
2024-08-26 22:31:18 +0530

    added Oauth

--- Controllers/ClgLogin-controller.js
+++ Controllers/ClgLogin-controller.js
@@ -33,7 +33,6 @@ const collegeLogin = async (req, res, next) => {
         );
 
         res.cookie('token', token, {
-            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'Strict',
             maxAge: 3600000,
