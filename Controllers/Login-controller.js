c5b89f84662444255560b9527fdbd9b14935a8c4
Shybash <shybashshaik@gmail.com>
2024-08-27 08:01:13 +0530

    added Oauth

--- Controllers/Login-controller.js
+++ Controllers/Login-controller.js
@@ -32,7 +32,6 @@ const login = async (req, res, next) => {
         );
 
         res.cookie('token', token, {
-            // httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
             sameSite: 'Strict', 
             maxAge: 3600000
