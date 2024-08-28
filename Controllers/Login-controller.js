e2c99f698f12228334f54f9cc8d250eec3995ca8
Shybash <shybashshaik@gmail.com>
2024-08-28 11:06:35 +0530

    added cookie

--- Controllers/Login-controller.js
+++ Controllers/Login-controller.js
@@ -28,10 +28,9 @@ const login = async (req, res, next) => {
         const personalInfo = await PersonalInfo.findOne({ userId: student._id });
 
     res.cookie('token', token, {
-  httpOnly: false, // This allows the cookie to be accessible via JavaScript
-  secure: process.env.NODE_ENV === 'production', // Ensures the cookie is only sent over HTTPS in production
-  sameSite: 'None', // Allows cross-site requests
-  maxAge: 3600000 // 1 hour
+        httpOnly: false, // Allow the cookie to be accessed by the frontend
+        secure: true, // Ensure cookie is sent only over HTTPS
+        maxAge: 3600000 // 1 hour in milliseconds
 });
 
         
