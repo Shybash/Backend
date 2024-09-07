c662a5ee6df5b311ae638fe628f13c150a1251df
Shybash <shybashshaik@gmail.com>
2024-09-07 14:41:05 +0530

    profle

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const passport = require('passport'); 
+const session = require('express-session');
+const passport = require('passport');
 require('dotenv').config();
-const passportConfig = require('./config/passport'); 
+const passportConfig = require('./config/passport');
 const connection = require('./config/db');
 const studentRoutes = require('./routes/StudentRoutes');
 const collegeRoutes = require('./routes/CollegeRouter');
@@ -20,9 +21,19 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    maxAge: 3600000,
+    sameSite: 'None',
+  },
+}));
 
-passportConfig(passport); 
-
+passportConfig(passport);
 app.use(passport.initialize());
 
 connection();
@@ -42,7 +53,7 @@ app.get('/auth/google/callback', passport.authenticate('google', { session: fals
         maxAge: 3600000,
         sameSite: 'None',
     });
-  
+
     res.redirect('https://frontend-clubhub-virid.vercel.app/student');
 });
 
