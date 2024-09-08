530128f9756f815c7a34503d1a8c4c5a69013c2c
Shybash <shybashshaik@gmail.com>
2024-09-08 12:12:09 +0530

    clg_login updated

--- Controllers/ClgRegister-controller.js
+++ Controllers/ClgRegister-controller.js
@@ -1,47 +1,55 @@
 const College = require('../models/College');
-const jwt = require('jsonwebtoken');
+const ClgInfo = require('../models/ClgInfo');
 const bcrypt = require('bcryptjs');
-const PersonalInfo=require('../models/ClgInfo');
 require('dotenv').config();
-const RegisterClg= async(req, res, next) => {
+
+const registerClg = async (req, res) => {
     try {
         const { username, email, password, confirmpassword } = req.body;
 
-        // Validate required fields
         if (!username || !email || !password || !confirmpassword) {
             return res.status(400).json({ error: "All fields are required" });
         }
-        
-       
+
         if (password.length < 6) {
             return res.status(400).json({ error: "Password should be at least 6 characters long" });
         }
 
-       
-        const exist = await College.findOne({ email });
-        if (exist) {
+        const existingCollege = await College.findOne({ email });
+        if (existingCollege) {
             return res.status(400).json({ error: "User already exists" });
         }
 
-      
         if (password !== confirmpassword) {
             return res.status(400).json({ error: "Passwords do not match" });
         }
 
-        const hashedPassword = await bcrypt.hashSync(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        const college = await College.create({ username, email, password: hashedPassword ,confirmpassword});
-
-        const personalInfo=new PersonalInfo({
-            userId:newUser._id,
+        const college = new College({
             username,
-            
-        })
+            email,
+            password: hashedPassword
+        });
 
-        res.status(201).json({ college });
+        await college.save();
+
+        const clgInfo = new ClgInfo({
+            userId: college._id,
+            username,
+            email
+        });
+
+        await clgInfo.save();
+
+        res.status(201).json({
+            message: "College registered successfully",
+            college,
+            clgInfo
+        });
     } catch (error) {
-        res.status(400).json({error:error.message});
+        res.status(500).json({ error: "Internal server error" });
     }
 };
 
-module.exports= RegisterClg;
+module.exports = registerClg;
