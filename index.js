b9964e946de61284e0f364fc4e881df8aa7d8df9
Shybash <shybashshaik@gmail.com>
2024-11-30 23:03:07 +0530

    updated Oauth

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/auth/google/callback', passport.authenticate('google', { session: fals
       maxAge: 3600000,
       sameSite: 'None',
   });
-  res.redirect('https://frontend-clubhub-virid.vercel.app/student?google=true');
+  res.redirect('https://frontend-clubhub-virid.vercel.app/student');
 });
 
 
