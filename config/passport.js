e6e3daa363060e740ec043865787a44d153e2584
Shybash <shybashshaik@gmail.com>
2024-12-12 14:15:06 +0530

    updated oauth

--- config/passport.js
+++ config/passport.js
@@ -25,7 +25,7 @@ module.exports = function (passport) {
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:8000/auth/google/callback',
+            callbackURL: 'https://clubhub-backend.vercel.app/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
